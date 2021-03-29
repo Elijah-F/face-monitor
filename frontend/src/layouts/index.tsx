32c406b1798614982441a99061085c7a483873d1
Elijah-F <Zhao_xiangshen@163.com>
2021-03-29 17:42:47 +0800

    refactor: mv Login.tsx to pages dir

--- frontend/src/layouts/index.tsx
+++ frontend/src/layouts/index.tsx
@@ -1,6 +1,5 @@
 import ProLayout, { DefaultFooter } from '@ant-design/pro-layout';
 import { SmileOutlined, CrownOutlined, RadarChartOutlined } from '@ant-design/icons';
-import Login from './Login';
 import React from 'react';
 import { Link } from 'umi';
 
@@ -22,23 +21,24 @@ const complexMenu = [
   },
 ];
 
-const BasicLayout: React.FC = (props) => (
-  <ProLayout
-    title="Face Monitor"
-    layout="top"
-    menuDataRender={() => complexMenu}
-    menuItemRender={(item: any, dom: React.ReactNode) => {
-      const { render, path } = item;
-      if (render) {
-        return render(dom);
-      }
-      return <Link to={path}>{dom}</Link>;
-    }}
-    footerRender={() => <DefaultFooter copyright="2021, fufuzhao. All right reserved" links={[]} />}
-  >
-    <Login />
-    <div style={{ minHeight: 280, padding: 24, background: '#fff' }}>{props.children}</div>
-  </ProLayout>
-);
+const BasicLayout: React.FC = (props) => {
+  return (
+    <ProLayout
+      title="Face Monitor"
+      layout="top"
+      menuDataRender={() => complexMenu}
+      menuItemRender={(item: any, dom: React.ReactNode) => {
+        const { render, path } = item;
+        if (render) {
+          return render(dom);
+        }
+        return <Link to={path}>{dom}</Link>;
+      }}
+      footerRender={() => <DefaultFooter copyright="2021, fufuzhao. All right reserved" links={[]} />}
+    >
+      <div style={{ minHeight: 280, padding: 24, background: '#fff' }}>{props.children}</div>
+    </ProLayout>
+  );
+};
 
 export default BasicLayout;
