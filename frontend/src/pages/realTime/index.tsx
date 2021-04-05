2924bb909debf4450c1b4fc6df20301f9cf98f2f
Elijah-F <Zhao_xiangshen@163.com>
2021-04-05 16:53:59 +0800

    feat(frontend): websocket for realTime

--- frontend/src/pages/realTime/index.tsx
+++ frontend/src/pages/realTime/index.tsx
@@ -1,40 +1,64 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 import { useInterval } from '@/utils/hooks';
-import { Button } from 'antd';
+import { Button, TimePicker, Divider, Space, Cascader, Card, Row } from 'antd';
+import { PoweroffOutlined } from '@ant-design/icons';
 
 const RealTime: React.FC = () => {
-  const webcamRef = useRef(null);
+  const webcamRef = useRef();
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
-  const [delay, setDelay] = useState<number>(500);
+  const [delay, setDelay] = useState<number>(100);
+  const [imgSrc, setImgSrc] = useState();
+  const [ws, setWs] = useState<WebSocket | null>(null);
 
   useInterval(
     () => {
       const imageSrc = webcamRef.current.getScreenshot();
-      // TODO: send imageSrc to backend using sigle connect
+      setImgSrc(imageSrc);
+      ws?.send(imageSrc);
     },
     isPlaying ? delay : null,
   );
 
+  useEffect(() => {
+    setWs(new WebSocket('ws://192.168.12.133:9527/real_time'));
+    if (ws) {
+      ws.onmessage = (evt) => {
+        console.log(evt);
+      };
+    }
+    return () => {
+      ws?.close();
+      setWs(null);
+    };
+  }, [setWs]);
+
   return (
     <>
-      <Button
-        type="primary"
-        onClick={() => {
-          setIsPlaying(true);
-        }}
-      >
-        开始
-      </Button>
-      <Button
-        type="primary"
-        onClick={() => {
-          setIsPlaying(false);
-        }}
-      >
-        结束
-      </Button>
-      <Webcam style={{ display: 'block' }} audio={false} ref={webcamRef} screenshotFormat="image/webp" />
+      <Space>
+        <TimePicker />
+        <Cascader />
+        <Button
+          type="primary"
+          danger={isPlaying}
+          icon={<PoweroffOutlined />}
+          onClick={() => {
+            setIsPlaying(!isPlaying);
+          }}
+        >
+          {isPlaying ? 'STOP' : 'START'}
+        </Button>
+      </Space>
+      <Divider />
+
+      <Row>
+        <Card>
+          <Webcam style={{ display: 'block' }} audio={false} ref={webcamRef} screenshotFormat="image/webp" />
+        </Card>
+        <Card>
+          <img src={imgSrc} />
+        </Card>
+      </Row>
     </>
   );
 };
